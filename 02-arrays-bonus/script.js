b75f9e74c0086c4fd5af0e5855dbb53528e5fc9d
Laassel <lassel.giovanni@gmail.com>
2024-10-17 10:32:50 +0200

    addition of bonus exercise with in-class correction

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,29 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.toReversed();
+console.log(teachers, reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+    const currentName = teachers[i];
+
+    if ( currentName.length >= 5) {
+      longNames.push(currentName)
+    }
+};
+console.log(longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const EdIndex = teachers.indexOf('Ed') //number [-1,length -1]
+if (EdIndex >= 0) {
+  
+  //rimouvere elemento all'indice EdIndex
+  teachers.splice(EdIndex,1)
+  
+}
+console.log(teachers);
